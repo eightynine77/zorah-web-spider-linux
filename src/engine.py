b64e451d314f17e5a44fee87fe6d5accbdeab051
eightynine77 <theskyisgrey80@proton.me>
2025-12-01 23:21:50 +0000

    fix the double-opening tab

--- src/engine.py
+++ src/engine.py
@@ -326,9 +326,6 @@ def start_server():
     from waitress import serve
     host = '127.0.0.1'
     port = 8080
-    app_url = f"http://{host}:{port}"
-    
-    Timer(1, open_browser, args=[app_url]).start()
     
     # Run the server
     serve(app, host=host, port=port)
